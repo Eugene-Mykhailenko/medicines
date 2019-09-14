8c6c1cf7d1ea1fa575a15e8d4a9a2a5c22bbdc8a
Eugene-Mykhailenko <potatoua@gmail.com>
2019-09-15 01:30:25 +0300

    fix(MedicinesListItem): refactoring

--- src/components/MedicinesListItem/index.js
+++ src/components/MedicinesListItem/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { collection } from "../../firebase";
+
 import { openModal } from "../../bus/AddForm/actions";
+
+function editModal(props){
+    const {data} = props;
+
+    props.dispatch(openModal(data.id, true));
+}
+
+function deleteItem(id){
+    collection.doc(id).delete()
+}
 
 function MedicinesListItem(props){
     const {data} = props;
 
-    function editModal(id){
-        props.dispatch(openModal(id, true));
-    }
-
-    function deleteItem(id){
-        collection.doc(id).delete()
-    }
-
     return (
-        <tr onDoubleClick={editModal.bind(null, data.id)}>
+        <tr onDoubleClick={editModal.bind(null, props)}>
             <th scope="row">{data.code}</th>
             <td>{data.name}</td>
             <td>{data.price}</td>
             <td style={{'textAlign': 'right'}}>
                 <button className="btn btn-outline-primary btn-sm m-1"
-                        onClick={editModal.bind(null, data.id)}
+                        onClick={editModal.bind(null, props)}
                 >
                     <i className="fa fa-edit"/>
                 </button>
